f2b9302a9c2347a103fd78d6c108b971d9c8368b
user001 <gacdav@mail.ru>
2025-06-25 10:14:07 +0300

    исправлено представление формы проверки фонда

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -5,30 +5,31 @@
     <h1>Проверка фонда</h1>
     <form action="{{ route('invApprove') }}" method="POST">
         @csrf
-        <div class="form-group">
-        <label for="simple-select">Кто проверяет:</label>
-            <select id="librarian">
-                <option value="" selected disabled>-- Библиотекарь --</option>
-                <option value="1">Иванов</option>
-                <option value="2">Петров</option>
-                <option value="3">Сидоров</option>
-            </select>
+        <div class="row">
+            <div class="col-md-3">Кто проверяет:</div>
+            <div class="col-md-3">{{ Auth::user()->name }}</div>
         </div>
-        <div class="form-group">
-        <label for="simple-select">Место хранения:</label>
-            <select id="librarian">
-                <option value="" selected disabled>-- Место хранения --</option>
-                <option value="1">Хранилище 1</option>
-                <option value="2">Хранилище 1</option>
-                <option value="3">Читальный зал</option>
-            </select>
+        <br>
+        <div class="row">
+            <div class="col-md-3">
+                <label for="simple-select">Место хранения:</label>
+            </div>
+            <div class="col-md-3">
+                <select id="librarian">
+                    <option value="" selected disabled>Место хранения</option>
+                    <option value="1">Хранилище 1</option>
+                    <option value="2">Хранилище 1</option>
+                    <option value="3">Читальный зал</option>
+                </select>
+            </div>
         </div>
+        <br>
         <div class="form-group">
             <label for="description">Инвентаный номер экземпляра</label>
             <input type="text" class="form-control" name="invnum" required>
         </div>
-        
-        <button type="submit" class="btn btn-success">Добавить</button>
+        <br>
+        <button type="submit" class="btn btn-success">Подтвердить</button>
     </form>
 </div>
 @endsection
