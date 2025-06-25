16d528d92189b6394ed177f0517b8ad71b09949c
user001 <gacdav@mail.ru>
2025-06-25 12:00:38 +0300

    inventory

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Подключаем класс irbis64
+require_once app_path('Http/Controllers/irbis_class.php');
+
 class InventoryController extends Controller
 {
     public function show(){
@@ -21,6 +24,24 @@ class InventoryController extends Controller
             'invNum' => 'required|string',
             'booksNum' => 'required|string',
         ]);
-        dd($validated);
+        //dd($validated);
+
+        $irbis = new \irbis64('127.0.0.1', 6666, '1', '1', 'IBIS');
+        if ($irbis->login()) {
+            $book = $irbis->records_search('IN='.$validated['invNum'], 10, 1, $format = '@all');//для вывода инфо о книге
+                if(!isset($book['records'])){//если запись не найдена по IN= ищем по INS=
+                    $book = $irbis->records_search('INS='.$validated['invNum'], 10, 1);
+                    $pref = 'INS=';
+                    if(!isset($book['records'])){//если запись не найдена по INS= ищем по EXU=
+                        $book = $irbis->records_search('EXU='.$validated['invNum'], 10, 1);
+                        $pref = 'EXU=';
+                        if(!isset($book['records'])){//запись не найдена
+                            dd("запись не найдена по префиксам IN, INS, EXU");
+                        }
+                    }
+                } 
+
+            dd($book);
+        }
     }
 }
